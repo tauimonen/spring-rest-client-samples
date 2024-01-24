08c91648debcb5195c18441312b9ac7eae7c309f
vgtoui <toni.uimonen@tuni.fi>
2024-01-24 22:27:34 +0200

    Create a Spring Boot Project

---
+++ src/main/java/dev/tau/restclientsamples/RestClientSamplesApplication.java
@@ -0,0 +1,13 @@
+package dev.tau.restclientsamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestClientSamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestClientSamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/tau/restclientsamples/RestClientSamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tau.restclientsamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestClientSamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
